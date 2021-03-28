6993346481f4ac72f738d0fc1937d9fc938725f7
PD-22 <72622627+PD-22@users.noreply.github.com>
2021-03-28 20:08:48 +0400

    replace on collision

--- sketch.js
+++ sketch.js
@@ -167,13 +167,14 @@ function move(ang, s) {
             vel.y += cos(radians(ang))
             vel.x += sin(radians(ang))
         }
-        let mag = sqrt(dir.x ** 2 + dir.y ** 2)
-        if (mag != 0) {
-            vel.x /= mag
-            vel.y /= mag
-        }
         dir.x = vel.x
         dir.y = vel.y
+    }
+
+    let mag = sqrt(dir.x ** 2 + dir.y ** 2)
+    if (mag != 0) {
+        vel.x /= mag
+        vel.y /= mag
     }
 
     if (speed != undefined) {
@@ -183,14 +184,16 @@ function move(ang, s) {
     vel.y /= 24
     vel.x /= 24
 
-    let cell
-    cell = getCell(pos.x, pos.y + vel.y + dir.y * rad, false)
-    if (getCellVal(cell) == 1) vel.y = 0
-    cell = getCell(pos.x + vel.x + dir.x * rad, pos.y, false)
-    if (getCellVal(cell) == 1) vel.x = 0
-
     pos.y += vel.y
     pos.x += vel.x
+
+    let cell
+    cell = getCell(pos.x, pos.y + dir.y * rad, false)
+    if (getCellVal(cell))
+        pos.y = cell.y + 0.5 - dir.y * 0.5 - dir.y * rad
+    cell = getCell(pos.x + dir.x * rad, pos.y, false)
+    if (getCellVal(cell))
+        pos.x = cell.x + 0.5 - dir.x * 0.5 - dir.x * rad
 
     if (drawMap) updateAng()
 }
